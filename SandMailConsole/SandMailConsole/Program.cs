f847a9b3a068671ba7afdb6a36a09fab70cb7d76
Евгений Тарунин <joes63t@gmail.com>
2018-09-03 12:15:11 +0300

    +

---
+++ SandMailConsole/SandMailConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace SandMailConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //формируем письмо 
+            MailMessage mm = new MailMessage("joess63t@yandex.ru", "ufrc@yandex.ru");
+            mm.Subject = "Заголовок письма";
+            mm.Body = "Тело письма";
+            mm.IsBodyHtml = false;                                  // не использовать html  в теле письма
+
+
+            //Авторизация на сервере smtp
+            SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25);
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = new NetworkCredential("joess63t@yandex.ru", "Alhimik163");
+
+
+            try
+            {
+                sc.Send(mm);
+            }
+            
+            catch 
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+        }
+    }
+}
